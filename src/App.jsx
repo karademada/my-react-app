51d0e7078005656bb95ff5382d12d3a58c7ef850
christophelazantsy <christophe.lazantsy@gmail.com>
2025-11-16 16:40:10 +0300

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box, Container } from '@chakra-ui/react'
 import ProductList from './features/products/ProductList'
 import ProductDetailPage from './features/products/ProductDetailPage'
 import UserAuth from './features/user/UserAuth'
 
 function App() {
-  const [selectedProduct, setSelectedProduct] = useState(null)
-
   return (
-    <Box>
-      <UserAuth />
-      <Container maxW="container.xl" py={6}>
-        {selectedProduct ? (
-          <ProductDetailPage 
-            productId={selectedProduct} 
-            onBack={() => setSelectedProduct(null)} 
-          />
-        ) : (
-          <ProductList onProductClick={setSelectedProduct} />
-        )}
-      </Container>
-    </Box>
+    <BrowserRouter>
+      <Box>
+        <UserAuth />
+        <Container maxW="container.xl" py={6}>
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+          </Routes>
+        </Container>
+      </Box>
+    </BrowserRouter>
   )
 }
 
